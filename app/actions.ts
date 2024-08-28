de624b871be0cdf78a3be4b5e223b90ba95d512f
Yong Sheng Tan <tanysh@pm.me>
2024-08-28 13:04:21 +0800

    Add token authorisation

--- app/actions.ts
+++ app/actions.ts
@@ -1,18 +1,20 @@
 "use server";
 
-import client from "@/lib/mongodb";
-import { ObjectId } from "mongodb";
-import { Todo } from "../app/interfaces";
-import { decodeJWT } from "./util";
+import client from '@/lib/mongodb';
+import { ObjectId } from 'mongodb';
+import { Todo } from '../app/interfaces';
+import { OAuth2Client } from 'google-auth-library';
 
-const collection = client.db("todo").collection<Todo>("todo");
+const collection = client.db('todo').collection<Todo>('todo');
 
-const authorise = (token : string) => {
-  return decodeJWT(token).email;
-};
+const authorise = async (token : string) : Promise<string> =>
+  (await new OAuth2Client().verifyIdToken({
+    idToken: token,
+    audience: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  })).getPayload()?.email || '';
 
 export const getTasks = async (token : string) : Promise<Todo[]> => {
-  const email = authorise(token);
+  const email = await authorise(token);
   try {
     const data = await collection
       .find({ owner: email })
@@ -27,27 +29,27 @@ export const getTasks = async (token : string) : Promise<Todo[]> => {
 };
 
 export const addTask = async (token : string, newTaskName : string) => {
-  const email = authorise(token);
-    const newTodo : Todo = {
-        _id: new ObjectId(),
-        name: newTaskName,
-        done: false,
-        created: new Date(),
-        owner: email,
-    };
-    console.log('Creating new todo:', newTodo);
-    await collection.insertOne(newTodo);
-    return await getTasks(email);
+  const email = await authorise(token);
+  const newTodo : Todo = {
+      _id: new ObjectId(),
+      name: newTaskName,
+      done: false,
+      created: new Date(),
+      owner: email,
+  };
+  console.log('Creating new todo:', newTodo);
+  await collection.insertOne(newTodo);
+  return await getTasks(token);
 };
 
 export const toggleTask = async (token: string, id: string, done: boolean) => {
   console.log(`Marking todo ${id} as ${done}`);
-  const email = authorise(token);
+  const email = await authorise(token);
   const data = await collection.updateOne({ _id: new ObjectId(id), owner: email }, { $set: { done } });
   return JSON.parse(JSON.stringify(data));
 };
 
 export const deleteTask = async (token : string, id: string) => {
-  const email = authorise(token);
+  const email = await authorise(token);
   await collection.deleteOne({ _id: new ObjectId(id), owner: email });
 };
